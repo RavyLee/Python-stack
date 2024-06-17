7f3a7f488dc662b67def93ac6b3d7a525a775ed7
RavyLee <81530509+RavyLee@users.noreply.github.com>
2024-06-17 17:42:36 +0300

    Add files via upload

---
+++ Урок №8. Списки/Задание №1/Задание__1.py
@@ -0,0 +1,17 @@
+﻿# Считываем число N
+N = int(input())
+
+# Создаем список для хранения чисел
+numbers = []
+
+# Считываем N чисел и добавляем их в список
+for _ in range(N):
+    number = int(input())
+    numbers.append(number)
+
+# Переворачиваем список
+numbers.reverse()
+
+# Выводим перевернутый список чисел
+for number in numbers:
+    print(number)
