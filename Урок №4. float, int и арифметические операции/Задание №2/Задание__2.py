a73382492a519eec4caee4c1c77d28c3395026cf
RavyLee <81530509+RavyLee@users.noreply.github.com>
2024-06-17 16:48:50 +0300

    Add files via upload

---
+++ Урок №4. float, int и арифметические операции/Задание №2/Задание__2.py
@@ -0,0 +1,29 @@
+﻿# Запрашиваем у пользователя пятизначное число
+number = int(input("Введите пятизначное число: "))
+
+# Выделяем необходимые разряды
+units = number % 10
+tens = (number // 10) % 10
+hundreds = (number // 100) % 10
+thousands = (number // 1000) % 10
+ten_thousands = (number // 10000) % 10
+
+# Возводим количество десятков в степень количества единиц
+result = tens ** units
+
+# Умножаем результат на количество сотен
+result *= hundreds
+
+# Находим разность между количеством десятков тысяч и количеством тысяч
+difference = ten_thousands - thousands
+
+# Делим результат на разность
+if difference != 0:
+    result /= difference
+else:
+    print("Ошибка: разность между десятками тысяч и тысячами равна нулю, деление невозможно.")
+    result = None
+
+# Выводим результат
+if result is not None:
+    print("Результат:", result)
