de8f5e378176bd8b27bfb8155c37ca9b1fa975ff
RavyLee <81530509+RavyLee@users.noreply.github.com>
2024-06-17 17:19:21 +0300

    Add files via upload

---
+++ Урок №7. Строки/Задание №1/Задание__1.py
@@ -0,0 +1,8 @@
+﻿# Считываем строку
+input_string = input()
+
+# Проверяем, является ли строка палиндромом
+if input_string == input_string[::-1]:
+    print("yes")
+else:
+    print("no")
---
+++ Урок №7. Строки/Задание №2/Задание__2.py
@@ -0,0 +1,11 @@
+﻿# Считываем строку
+input_string = input()
+
+# Разбиваем строку на слова
+words = input_string.split()
+
+# Соединяем слова обратно в строку с одним пробелом между ними
+result_string = ' '.join(words)
+
+# Выводим изменённую строку
+print(result_string)
