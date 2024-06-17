a7a87b7da68d026ebf82195f1ac9013295ead8ca
RavyLee <81530509+RavyLee@users.noreply.github.com>
2024-06-17 17:57:05 +0300

    Add files via upload

---
+++ Урок №9. Множества/Задание №2/Задание__2.py
@@ -0,0 +1,25 @@
+﻿# Считываем количество чисел в первом списке
+N = int(input())
+
+# Считываем первый список чисел
+first_list = []
+for _ in range(N):
+    first_list.append(int(input()))
+
+# Считываем количество чисел во втором списке
+M = int(input())
+
+# Считываем второй список чисел
+second_list = []
+for _ in range(M):
+    second_list.append(int(input()))
+
+# Преобразуем списки в множества
+first_set = set(first_list)
+second_set = set(second_list)
+
+# Находим пересечение двух множеств
+intersection = first_set.intersection(second_set)
+
+# Выводим количество элементов в пересечении
+print(len(intersection))
---
+++ Урок №9. Множества/Задание №3/Задание__3.py
@@ -0,0 +1,13 @@
+﻿# Считываем последовательность чисел через пробел
+numbers = list(map(int, input().split()))
+
+# Создаем пустое множество для отслеживания встреченных чисел
+seen_numbers = set()
+
+# Проходим по каждому числу в последовательности
+for number in numbers:
+    if number in seen_numbers:
+        print("YES")
+    else:
+        print("NO")
+        seen_numbers.add(number)
