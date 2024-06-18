04a0c8e4571a888f4356c34fc9c73e6f8d37d914
RavyLee <81530509+RavyLee@users.noreply.github.com>
2024-06-18 14:25:39 +0300

    Add files via upload

---
+++ Урок №14. Рекурсия/Задание №1/Задание__1.py
@@ -0,0 +1,16 @@
+﻿def print_list_recursively(my_list, index=0):
+    # Базовый случай: если индекс выходит за пределы списка, выводим сообщение
+    if index >= len(my_list):
+        print("Конец списка")
+        return
+    # Вывод текущего элемента
+    print(my_list[index])
+    # Рекурсивный вызов функции для следующего элемента
+    print_list_recursively(my_list, index + 1)
+
+# Исходный список
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+# Вызов функции для печати элементов списка
+print_list_recursively(my_list)
+
