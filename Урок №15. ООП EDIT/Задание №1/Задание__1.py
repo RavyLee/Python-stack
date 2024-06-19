e2bf8ad7c22ee3f69c36f1f4445358825d7f4364
RavyLee <81530509+RavyLee@users.noreply.github.com>
2024-06-19 17:30:30 +0300

    Add files via upload

---
+++ Урок №15. ООП EDIT/Задание №1/Задание__1.py
@@ -0,0 +1,11 @@
+﻿class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+# Создание объекта класса Transport
+transport = Transport("Renaul Logan", 180, 12)
+
+# Вывод информации об объекте
+print(f"Название автомобиля: {transport.name} Скорость: {transport.max_speed} Пробег: {transport.mileage}")
---
+++ Урок №15. ООП EDIT/Задание №2/Задание__2.py
@@ -0,0 +1,19 @@
+﻿class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"Вместимость одного автобуса {self.name} {capacity} пассажиров"
+
+class Autobus(Transport):
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity)
+
+# Создание объекта класса Autobus
+autobus = Autobus("Renaul Logan", 180, 12)
+
+# Вывод информации о вместимости автобуса
+print(autobus.seating_capacity())
+
